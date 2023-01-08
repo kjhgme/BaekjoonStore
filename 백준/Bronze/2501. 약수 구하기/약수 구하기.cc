08da50b5553ee491b0781c35f26539255b6c5181
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-01-09 05:07:55 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int cnt{};
+	for (int i = 1; i <= n; ++i)
+	{
+		if (n % i == 0)
+			cnt++;
+
+		if (cnt == k) {
+			cout << i;
+			break;
+		}
+	}
+
+	if (cnt != k)
+		cout << 0;
+}
