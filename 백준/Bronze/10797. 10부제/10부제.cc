bb54252f79e3833e7cf1aa6bfdfed232f2a4ff28
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-01-11 19:17:35 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int x{};
+	int cnt{};
+	for (int i = 0; i < 5; ++i)
+	{
+		cin >> x;
+		if (x == n)
+			cnt++;
+	}
+	cout << cnt;
+}
