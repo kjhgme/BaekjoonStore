f52e1ae6b5b356976e8c3442de4f1d994cb2f5f5
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-09-05 11:14:12 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T{};  // 테스트 케이스의 개수
+
+    int a{}, b{};   // 두 정수 a, b
+
+    cin >> T;
+
+    // 테스트 케이스만큼 두 정수의 합 출력하기
+    for (int i = 0; i < T; ++i)
+    {
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a << " + " << b << " = " << a + b << endl;
+    }
+}
