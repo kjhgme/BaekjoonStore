a52e08aac410249036ac6f706854e7622940e173
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-09-14 21:40:28 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (char i = 'a'; i <= 'z'; i++) {
+		cout << (int)s.find(i) << ' ';
+	}
+
+
+	return 0;
+}
