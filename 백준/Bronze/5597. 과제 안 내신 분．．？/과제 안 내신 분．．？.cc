1f39885854c08fab8852c8b6ed8e777ec3886e7b
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-09-15 20:30:30 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[30]{0, };
+	int n{};
+
+	for (int i = 0; i < 28; ++i)
+	{
+		cin >> n;
+
+		a[n - 1] = n;
+	}
+
+	for (int i = 0; i < 30; ++i)
+	{
+		if (a[i] == 0)
+			cout << i+1 << endl;
+	}
+}
