9780ac91827f97aa288760302f774cc75d72a93c
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-09-15 20:51:51 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); ++i)
+	{
+		if ('a' <= s[i] && s[i] <= 'z')
+			s[i] = toupper(s[i]);
+		else
+			s[i] = tolower(s[i]);
+	}
+
+	cout << s;
+}
