f07583114ba56712b74facfff07244a154e42251
kjhgme <64467890+kjhgme@users.noreply.github.com>
2023-10-15 16:37:29 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#define r 31
 #define M 1234567891
 using namespace std;
 
@@ -6,15 +7,16 @@ int main()
 {
 	int l{};
 	string s{};
-	long long hash{}, r{1};
+	long long hash{}, R{1};
 
 	cin >> l;
 	cin >> s;
 
 	for (int i = 0; i < s.size(); ++i)
 	{
-		hash += ((s[i] - 'a' + 1) * r) % M;
-		r = (r * 31) % M;
+		hash += ((s[i] - 'a' + 1) * R) % M;
+		hash %= M;
+		R = (R * r) % M;
 	}
 
 	printf("%lld\n", hash);
