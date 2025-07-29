d72b469681ff6a9182551651e7a09c555430f0f2
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-07-29 23:14:50 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using t3 = tuple<int, int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    int a, b;
+    cin >> a >> b;
+
+    cout << b * 2 - a;
+}
