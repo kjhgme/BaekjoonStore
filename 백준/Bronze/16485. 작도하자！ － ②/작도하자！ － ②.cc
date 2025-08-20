21a61facda4555fc497e6a48cdff1a01a959daca
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-08-20 18:02:53 +0900

    [Bronze II] Title: 작도하자! - ②, Time: 0 ms, Memory: 2020 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using t3 = tuple<int, int, int>;
+
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    double c, b;
+    cin >> c >> b;
+
+    cout << fixed;
+    cout.precision(10);
+    cout << c / b;
+}
