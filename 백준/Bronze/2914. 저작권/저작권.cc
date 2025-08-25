962b9cedb358818251788bf9135663009ffdaa96
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-08-25 21:12:34 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, i;
+    cin >> a >> i;
+
+    cout << a * (i - 1) + 1;
+}
