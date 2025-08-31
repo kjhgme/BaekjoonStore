b202854f1072b953fbc23e5a6b80ee698d1f496a
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-08-31 22:06:15 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <math.h>
+#include <set>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << "LoveisKoreaUniversity ";
+    }
+}
