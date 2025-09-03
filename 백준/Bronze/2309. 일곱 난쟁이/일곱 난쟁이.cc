c007431affc7d941e7d89759f772e56b8b9c8f7b
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-04 02:07:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -8,40 +8,44 @@ using namespace std;
 
 #define all(x) (x).begin(), (x).end()
 
-vector<int> v(9);
-vector<int> vv;
-bool found = false;
-
-void dfs(int start, int depth, int sum) {
-    if (depth == 7) {
-        if (sum == 100) {
-            sort(vv.begin(), vv.end());
-
-            for (int x : vv) cout << x << "\n";
-            found = true;
-        }
-        return;
-    }
-
-    if (found) return;
-
-    for (int i = start; i < 9; ++i) {
-        vv.push_back(v[i]);
-        dfs(i + 1, depth + 1, sum + v[i]);
-        vv.pop_back();
-    }
-}
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    vector<int> v(9);
 
     for (int i = 0; i < 9; ++i)
     {
         cin >> v[i];
     }
 
-    dfs(0, 0, 0);
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = i + 1; j < 9; ++j) {
+            for (int k = j + 1; k < 9; ++k) {
+                for (int l = k + 1; l < 9; ++l) {
+                    for (int m = l + 1; m < 9; ++m) {
+                        for (int n = m + 1; n < 9; ++n) {
+                            for (int o = n + 1; o < 9; ++o) {
+                                int sum = v[i] + v[j] + v[k] + v[l] + v[m] + v[n] + v[o];
+
+                                if (sum == 100) {
+                                    cout << v[i] << "\n"
+                                        << v[j] << "\n"
+                                        << v[k] << "\n"
+                                        << v[l] << "\n"
+                                        << v[m] << "\n"
+                                        << v[n] << "\n"
+                                        << v[o] << "\n";
+                                    return 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
