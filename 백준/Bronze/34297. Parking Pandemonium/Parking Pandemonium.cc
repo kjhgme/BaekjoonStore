04f2a4575c5e9e029853715711ec05ffb94ddbf7
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-10 23:49:06 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int m, k, c;
+    cin >> m;
+    cin >> k;
+    cin >> c;
+
+    cout << m * c;
+}
