0b9e8f51ffb2a0f9ad5f9cc8ef4a5cd985251594
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-14 21:39:16 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cout << "  ___  ___  ___" << endl;
+    cout << "  | |__| |__| |" << endl;
+    cout << "  |           |" << endl;
+    cout << "   \\_________/" << endl;
+    cout << "    \\_______/" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |_____|" << endl;
+    cout << "  __/       \\__" << endl;
+    cout << " /             \\" << endl;
+    cout << "/_______________\\" << endl;
+}
