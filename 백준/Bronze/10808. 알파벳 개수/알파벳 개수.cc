0a0bde41cd2385b24a0316001d6799b8b6bde8c0
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-18 01:06:38 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -12,28 +12,15 @@ int main(void) {
 	string s;
 	cin >> s;
 
-	vector<pair<char, int>> v;
-
-
 	for (char i = 'a'; i <= 'z'; ++i)
 	{
-		v.push_back(make_pair(i, 0));
-	}
-
-	for (auto c : s)
-	{
-		for (int i = 0; i < v.size(); ++i)
+		int cnt = 0;
+		for (auto c : s)
 		{
-			if (v[i].first == c)
-			{
-				v[i].second++;
-				break;
-			}
+			if (c == i)
+				cnt++;
 		}
-	}
 
-	for (int i = 0; i < v.size(); ++i)
-	{
-		cout << v[i].second << " ";
+		cout << cnt << " ";
 	}
 }
