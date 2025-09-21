4706e145caa44859b7d04cc09b51f2e846dcd2aa
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-21 19:47:54 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+
+    cout << n << '\n';
+
+    cout << 1 << '\n';
+}
