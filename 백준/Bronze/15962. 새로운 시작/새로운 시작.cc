047989e88a089d47ed31d62bd7e1e4b409c2733f
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-09-24 15:24:13 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "파이팅!!";
+}
