cd4988a267150324cdd0a37632197c762712ba8a
kjhgme <64467890+kjhgme@users.noreply.github.com>
2025-11-06 16:38:42 +0900

    [Silver IV] Title: 스케이트 연습, Time: 0 ms, Memory: 2160 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+    
+    v[v.size() - 1] = 1;
+
+
+    for (int i = v.size() - 1; i >= 0; --i)
+    {
+        if (i != v.size() - 1)
+        {
+            if (v[i] > v[i + 1] + 1)
+            {
+                v[i] = v[i + 1] + 1;
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        sum += v[i];
+    }
+
+    cout << sum;
+}
