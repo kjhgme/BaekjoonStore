38d2dc55b7cea65be3596e00db81f9459a0a3da2
kjhgme <64467890+kjhgme@users.noreply.github.com>
2026-01-07 22:12:58 +0900

    [Bronze IV] Title: 새천년관, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+// #include <bits/stdc++.h>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "nkugwan";
+}
