cddd904369b46a1c0902658e1d3b13095c610bbd
kjhgme <64467890+kjhgme@users.noreply.github.com>
2026-05-08 15:29:54 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 1.88 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if ('a' <= str[i] && str[i] <= 'z')
+            str[i] -= 32;
+        else
+            str[i] += 32;
+    }
+
+    cout << str;
+    
+    return 0;
+}
